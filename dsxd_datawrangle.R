42bdbb82e19428d44a785c8798df0a191d574175
statasaurus <55274484+statasaurus@users.noreply.github.com>
2021-06-30 18:46:05 +0100

    Adding in the plots

--- dsxd_datawrangle.R
+++ dsxd_datawrangle.R
@@ -1,5 +1,6 @@
 library(RPostgreSQL)
 library(tidyverse)
+library(lubridate)
 #Loading required package: DBI
 drv <- dbDriver('PostgreSQL')
 
@@ -38,3 +39,80 @@ study_tbl <- dbGetQuery(con, "SELECT std.nct_id, std.study_type, std.enrollment,
 
 overall <- wide_g_tbl %>%
    inner_join(study_tbl, by = "nct_id")
+
+
+
+graph_df <- overall %>%
+   mutate(year = year(start_date),
+          f_pct = Female/enrollment,
+          m_pct = Male/enrollment) %>%
+   ungroup() %>%
+   distinct(nct_id, .keep_all = TRUE) %>%
+   group_by(year) %>%
+   summarise(n_studies = n_distinct(nct_id),
+             f_mean = mean(f_pct, na.rm = TRUE),
+             f_med = median(f_pct, na.rm = TRUE),
+             m_mean = mean(m_pct, na.rm = TRUE),
+             m_med = median(m_pct, na.rm = TRUE))
+graph_df %>%
+   pivot_longer(cols = f_mean:m_med,
+                names_to = c("gender", "measure"),
+                names_pattern = "(.)_(.*)",
+                values_to = "count") %>%
+   filter(year > 2000, measure == "med") %>%
+   ggplot(aes(x = year, y = n_studies*count, fill = gender)) +
+   geom_bar(stat = "identity") +
+   ylab("Number of studie\nMedian percentage of Females") +
+   theme_bw()
+
+
+graph_df %>%
+   pivot_longer(cols = f_mean:m_med,
+                names_to = c("gender", "measure"),
+                names_pattern = "(.)_(.*)",
+                values_to = "count") %>%
+   filter(year > 2000, measure == "mean") %>%
+   ggplot(aes(x = year, y = n_studies*count, fill = gender)) +
+   geom_bar(stat = "identity") +
+   ylab("Number of studie\nMean percentage of Females") +
+   theme_bw()
+
+
+
+
+
+graph_df <- overall %>%
+   filter(study_type == "Interventional") %>%
+   mutate(year = year(start_date),
+          f_pct = Female/enrollment,
+          m_pct = Male/enrollment) %>%
+   ungroup() %>%
+   distinct(nct_id, .keep_all = TRUE) %>%
+   group_by(year) %>%
+   summarise(n_studies = n_distinct(nct_id),
+             f_mean = mean(f_pct, na.rm = TRUE),
+             f_med = median(f_pct, na.rm = TRUE),
+             m_mean = mean(m_pct, na.rm = TRUE),
+             m_med = median(m_pct, na.rm = TRUE))
+graph_df %>%
+   pivot_longer(cols = f_mean:m_med,
+                names_to = c("gender", "measure"),
+                names_pattern = "(.)_(.*)",
+                values_to = "count") %>%
+   filter(year > 2000, measure == "med") %>%
+   ggplot(aes(x = year, y = n_studies*count, fill = gender)) +
+   geom_bar(stat = "identity", position = "dodge") +
+   ylab("Number of studie\nMedian percentage of Females") +
+   theme_bw()
+
+
+graph_df %>%
+   pivot_longer(cols = f_mean:m_med,
+                names_to = c("gender", "measure"),
+                names_pattern = "(.)_(.*)",
+                values_to = "count") %>%
+   filter(year > 2000, measure == "mean") %>%
+   ggplot(aes(x = year, y = n_studies*count, fill = gender)) +
+   geom_bar(stat = "identity", position = "dodge") +
+   ylab("Number of studie\nMean percentage of Females") +
+   theme_bw()
